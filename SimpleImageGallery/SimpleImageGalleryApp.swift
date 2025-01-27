2e88fde9bcae29b82247724558f28ac599f62f36
Francisco Casillas <chiccasillas@gmail.com>
2025-01-27 10:39:39 -0600

    Initial Commit

---
+++ SimpleImageGallery/SimpleImageGalleryApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleImageGalleryApp.swift
+//  SimpleImageGallery
+//
+//  Created by Francisco Jean on 27/01/25.
+//
+
+import SwiftUI
+
+@main
+struct SimpleImageGalleryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
