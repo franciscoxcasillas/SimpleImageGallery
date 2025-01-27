e5ec5fd76e70f16e9e60197767fd004a1667173f
Francisco Casillas <chiccasillas@gmail.com>
2025-01-27 11:46:02 -0600

    Add web images and SF Symbols

--- SimpleImageGallery/ContentView.swift
+++ SimpleImageGallery/ContentView.swift
@@ -12,6 +12,22 @@ struct ContentView: View {
         VStack {
             Text("My gallery")
                 .font(.title)
+                .padding()
+            HStack{
+                Image(systemName: "star.fill")
+                    .foregroundColor(.yellow)
+                Image(systemName: "powersleep")
+                    .foregroundColor(.blue)
+                Image(systemName: "cloud.fill")
+                    .foregroundColor(.green)
+            }
+            AsyncImage(url: URL(string: "https://64.media.tumblr.com/c63505278caaf5384081bb7691dcab88/f8a646d0e2af3d70-11/s1280x1920/8d9ac10000327dce113119f7f545713670c7a169.jpg")) { image in image
+                            .resizable()
+                            .scaledToFit()
+                    } placeholder: {
+                        ProgressView()
+                    }
+                    .frame(width: 200, height: 200)
             Image("Mountain")
                 .resizable()
                 .scaledToFit()
